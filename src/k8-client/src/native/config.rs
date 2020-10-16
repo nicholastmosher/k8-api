fe7ead33dc4700cb15963bd016e5ef3ad3b37850
Nick Mosher <nicholastmosher@gmail.com>
2020-10-16 08:14:13 -0400

    WIP: Pkcs12 conversion

--- src/k8-client/src/native/config.rs
+++ src/k8-client/src/native/config.rs
@@ -1,4 +1,5 @@
-use std::io::Error as IoError;
+use std::io::{Error as IoError, Write};
+use std::io::ErrorKind;
 use std::path::Path;
 
 use isahc::config::CaCertificate;
@@ -7,23 +8,40 @@ use isahc::config::PrivateKey;
 use isahc::HttpClient;
 use isahc::HttpClientBuilder;
 use isahc::config::Configurable;
+use openssl::x509::X509;
+use openssl::pkcs12::Pkcs12;
 use tracing::debug;
 
 use crate::cert::ConfigBuilder;
 use crate::ClientConfigBuilder;
 use crate::ClientError;
+use openssl::pkey::PKey;
 
 pub type IsahcBuilder = ClientConfigBuilder<IsahcConfigBuilder>;
 
 /// load client certificate
-fn load_client_certificate<P>(client_crt_path: P, client_key_path: P) -> ClientCertificate
+fn load_client_certificate<P>(client_crt_path: P, client_key_path: P) -> Result<ClientCertificate, IoError>
 where
     P: AsRef<Path>,
 {
-    ClientCertificate::pem_file(
-        client_crt_path.as_ref().to_owned(),
-        PrivateKey::pem_file(client_key_path.as_ref().to_owned(), String::from("")),
-    )
+    let client_cert = std::fs::read(client_crt_path)?;
+    let private_key = std::fs::read(client_key_path)?;
+
+    let x509 = X509::from_pem(&client_cert)
+        .map_err(|e| IoError::new(ErrorKind::InvalidData, format!("Client Cert invalid PEM: {}", e)))?;
+    let pkey = PKey::private_key_from_pem(&private_key)
+        .map_err(|e| IoError::new(ErrorKind::InvalidData, format!("Private Key invalid PEM: {}", e)))?;
+    let p12 = Pkcs12::builder().build("", "kubeconfig", &pkey, &x509)
+        .map_err(|e| IoError::new(ErrorKind::InvalidData, format!("Failed to create Pkcs12: {}", e)))?;
+    let p12_der = p12.to_der()
+        .map_err(|e| IoError::new(ErrorKind::Other, format!("Failed to write p12 to der: {}", e)))?;
+
+    let p12_path = std::env::temp_dir().join("k8.p12");
+    let mut p12_file = std::fs::File::create(&p12_path)?;
+    p12_file.write_all(&p12_der)?;
+    p12_file.flush()?;
+
+    Ok(ClientCertificate::p12_file(&p12_path, "".to_string()))
 }
 
 fn load_ca_certificate<P>(ca_path: P) -> CaCertificate
@@ -66,7 +84,7 @@ impl ConfigBuilder for IsahcConfigBuilder {
     where
         P: AsRef<Path>,
     {
-        let client_certificate = load_client_certificate(client_crt_path, client_key_path);
+        let client_certificate = load_client_certificate(client_crt_path, client_key_path)?;
         debug!("retrieved client certs from kubeconfig");
         let inner = self.0.ssl_client_certificate(client_certificate);
         Ok(Self(inner))
